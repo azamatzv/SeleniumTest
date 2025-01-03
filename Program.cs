a7ca4491180582392e23682b711f10a4e3535317
Azamat <azamatzokirov45@gmail.com>
2025-01-03 14:41:41 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTest;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        try
+        {
+            driver.Navigate().GoToUrl("http://asilmedia.org/films/tarjima_kinolar/");
+            driver.Manage().Window.Maximize();
+
+            var filmLinks = driver.FindElements(By.XPath("//*[@id='dle-content']/article//a"));
+            List<string> filmUrls = new List<string>();
+
+            foreach (var link in filmLinks)
+            {
+                string url = link.GetAttribute("href");
+                if (!string.IsNullOrEmpty(url))
+                {
+                    filmUrls.Add(url);
+                }
+            }
+
+            foreach (var url in filmUrls)
+            {
+                driver.Navigate().GoToUrl(url);
+
+                Thread.Sleep(5000);
+
+                driver.Navigate().Back();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Xatolik yuz berdi: " + ex.Message);
+        }
+    }
+}
