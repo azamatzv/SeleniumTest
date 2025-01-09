bf564b60e2b90ff117214d50b317c0630f1be024
Azamat <azamatzokirov45@gmail.com>
2025-01-09 18:25:33 +0500

    .

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using OpenQA.Selenium;
+﻿/*using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
 namespace SeleniumTest;
@@ -7,6 +7,8 @@ public class Program
 {
     static void Main(string[] args)
     {
+        Console.ForegroundColor = ConsoleColor.Green;
+        //87 page dan boshlab qo'shaman
         string connectionString = "Host=localhost;Port=5432;Database=postgres;Username=postgres;Password=0932";
         var filmDatabaseService = new FilmDatabaseService(connectionString);
 
@@ -14,7 +16,7 @@ public class Program
 
         try
         {
-            driver.Navigate().GoToUrl("http://asilmedia.org/films/tarjima_kinolar/page/5/");
+            driver.Navigate().GoToUrl("http://asilmedia.org/films/tarjima_kinolar/page/91/");
             driver.Manage().Window.Maximize();
 
             bool hasNextPage = true;
@@ -55,11 +57,11 @@ public class Program
 
                     filmDatabaseService.AddFilm(film);
 
-                    Console.WriteLine($"Film '{film.Title}' bazaga qo'shildi.");
-                    Console.WriteLine("-----------------------------------------");
+                    Console.WriteLine($"Film  '{film.Title}'  qo'shildi.");
+                    Console.WriteLine("------------------------------------------------------------------------------------------------");
                 }
 
-                driver.Navigate().Back();
+                //driver.Navigate().Back();
 
                 var nextButton = driver.FindElements(By.XPath("//a[contains(@class, 'next')]"));
                 if (nextButton.Count > 0)
@@ -102,5 +104,113 @@ public class Program
             return "Mavjud emas";
         }
     }
-}
+}*/
 
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTest;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+
+        string connectionString = "Host=localhost;Port=5432;Database=postgres;Username=postgres;Password=0932";
+        var filmDatabaseService = new FilmDatabaseService(connectionString);
+
+        IWebDriver driver = new ChromeDriver();
+
+        try
+        {
+            int page = 311; //311  dan boshlab qo'shaman
+            bool hasNextPage = true;
+
+            while (hasNextPage)
+            {
+                string url = $"http://asilmedia.org/films/tarjima_kinolar/page/{page}/";
+                Console.WriteLine($"Sahifa ochilmoqda: {url}");
+                driver.Navigate().GoToUrl(url);
+                driver.Manage().Window.Maximize();
+
+                // Maqolalarni olish
+                var articles = driver.FindElements(By.XPath("//*[@id='dle-content']/article"));
+                if (articles.Count == 0)
+                {
+                    Console.WriteLine("Sahifada maqolalar topilmadi.");
+                    break;
+                }
+
+                List<string> filmUrls = new List<string>();
+                foreach (var al in articles)
+                {
+                    string articleUrl = al.FindElement(By.TagName("a")).GetAttribute("href");
+                    if (!string.IsNullOrEmpty(articleUrl))
+                    {
+                        filmUrls.Add(articleUrl);
+                    }
+                }
+
+                // Har bir film uchun ma'lumot olish
+                foreach (var filmUrl in filmUrls)
+                {
+                    driver.Navigate().GoToUrl(filmUrl);
+                    System.Threading.Thread.Sleep(2000);
+
+                    var film = new Film
+                    {
+                        Title = GetElementText(driver, "//h1"),
+                        ImageUrl = GetElementAttribute(driver, "//article//img", "src"),
+                        Description = GetElementText(driver, "//*[@id=\"dle-content\"]/article/div/div[2]/div[1]/div[1]/div[1]"),
+                        Genres = GetElementText(driver, "//*[@id=\"dle-content\"]/article/div/div[2]/div[1]/div[1]/div[2]/div[1]/span[2]"),
+                        Year = GetElementText(driver, "//*[@id=\"dle-content\"]/article/div/div[2]/div[1]/div[2]/div/div[1]/span[2]/a"),
+                        Country = GetElementText(driver, "//*[@id=\"dle-content\"]/article/div/div[2]/div[1]/div[2]/div/div[2]/span[2]/a"),
+                        ImdbRating = GetElementText(driver, "//*[@id=\"dle-content\"]/article/div/div[2]/div[2]/div/div[3]/div[3]/span[2]"),
+                        KinopoiskRating = GetElementText(driver, "//span[contains(text(), 'Кинопоиск')]/following-sibling::span"),
+                        Director = GetElementText(driver, "//*[@id=\"dle-content\"]/article/div/div[2]/div[1]/div[1]/div[2]/div[2]/span[2]/a"),
+                        Actors = GetElementText(driver, "//*[@id=\"dle-content\"]/article/div/div[2]/div[1]/div[1]/div[2]/div[3]"),
+                        DownloadLink = GetElementAttribute(driver, "//a[contains(text(), 'Скачать')]", "href")
+                    };
+
+                    filmDatabaseService.AddFilm(film);
+
+                    Console.WriteLine($"Film '{film.Title}' qo'shildi.");
+                    Console.WriteLine("------------------------------------------------------------------------------------------------");
+                }
+
+                page++;
+                Console.WriteLine($"Keyingi sahifaga o'tish: {page}");
+            }
+        }
+        finally
+        {
+            driver.Quit();
+        }
+    }
+
+    static string GetElementText(IWebDriver driver, string xpath)
+    {
+        try
+        {
+            return driver.FindElement(By.XPath(xpath)).Text;
+        }
+        catch
+        {
+            return "Mavjud emas";
+        }
+    }
+
+    static string GetElementAttribute(IWebDriver driver, string xpath, string attribute)
+    {
+        try
+        {
+            return driver.FindElement(By.XPath(xpath)).GetAttribute(attribute);
+        }
+        catch
+        {
+            return "Mavjud emas";
+        }
+    }
+}
